85fbe184f0ca1bab2851d0800725d51c38dafc0e
Saurabh Kumar <106867366+desmond3th@users.noreply.github.com>
2023-04-19 18:55:45 +0530

    Update astronomy.py

--- astronomy.py
+++ astronomy.py
@@ -1,25 +1,38 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
 
-import tensorflow as tf 
-from tensorflow.keras import datasets, layers, models 
-import matplotlib.pyplot as plt 
-
+# Load the Fashion-MNIST dataset
 (X_train, y_train), (X_test, y_test) = datasets.fashion_mnist.load_data()
 
+# Normalize the pixel values of the images to be between 0 and 1
 X_train = X_train / 255.0
 X_test = X_test / 255.0
 
+# Reshape the data to add a channel dimension for the CNN
+X_train = X_train.reshape(-1, 28, 28, 1)
+X_test = X_test.reshape(-1, 28, 28, 1)
+
+# Define the CNN model
 model = models.Sequential([
-  layers.Flatten(input_shape=(28, 28)),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(10)
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10)
 ])
 
+# Compile the model
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
+# Train the model
 model.fit(X_train, y_train, epochs=5)
 
+# Evaluate the model on the test set
 test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
 
 print('\nTest accuracy:', test_acc)
